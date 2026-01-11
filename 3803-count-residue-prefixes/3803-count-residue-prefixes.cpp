e2a932bce1ca2f9f188923abbcee66bec2984025
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-01-11 19:41:15 +0530

    Time: 0 ms (100%), Space: 10 MB (50.74%) - LeetHub

---
+++ 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int residuePrefixes(string s) {
+        int n=s.size();
+        set<int> st;
+        int c=0;
+        for(int i=0;i<n;i++)
+        {   
+           
+           st.insert(s[i]);
+           if(st.size()==(i+1)%3)
+           {
+            c+=1;
+           }
+
+        }
+        return c;
+    }
+}; 
