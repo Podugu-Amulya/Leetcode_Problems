293d85a2b53982109ce177732e74158ef4496bf5
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-01-23 15:08:52 +0530

    01-23-2026 2932-maximum-strong-pair-xor-i .cpp Easy

---
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int m=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums.size();j++)
+            {
+                if(abs(nums[i]-nums[j]) <= min(nums[i],nums[j]))
+                {
+                    m=max(m,nums[i] ^ nums[j]);
+                }
+            }
+        }
+        return m;
+        
+    }
+};
