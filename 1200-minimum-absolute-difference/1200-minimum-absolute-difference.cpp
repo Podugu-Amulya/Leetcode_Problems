09c421814d32d9ba79c6d570acb4fee17c8c7fa8
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-01-26 20:19:17 +0530

    01-26-2026 1200-minimum-absolute-difference .cpp Easy

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        vector<vector<int>> res;
+        sort(arr.begin(),arr.end());
+        int m=INT_MAX;
+        for(int i=1;i<arr.size();i++)
+        {
+             m=min(m,(arr[i]-arr[i-1]));
+        }
+        for(int i=1;i<arr.size();i++)
+        {
+            
+                if((arr[i]-arr[i-1])==m)
+                {
+                    res.push_back({arr[i-1],arr[i]});
+                }
+             
+        }
+        return res;
+        
+    }
+};
