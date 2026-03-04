132179521f38101b4bf0c177070a2c66c800d2cc
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-03-04 13:52:22 +0530

    03-04-2026 2544-alternating-digit-sum .cpp Easy

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        int sum=0;
+        string s=to_string(n);
+        for(int i=0;i<s.size();i++)
+        {
+            int k=s[i]-'0';
+            if(i%2==0)
+            {
+            sum=sum+k;
+            }
+            else
+            {
+            sum=sum-k;
+            }
+        }
+        return sum;
+        
+    }
+};
