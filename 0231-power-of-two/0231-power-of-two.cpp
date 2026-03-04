88d3e17671598a812a38fb8b8d10444a64f73429
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-03-04 13:44:49 +0530

    03-04-2026 0231-power-of-two .cpp Easy

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        for(int i=0;i<50;i++)
+        {
+            if(pow(2,i)==n)
+            {
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+};
