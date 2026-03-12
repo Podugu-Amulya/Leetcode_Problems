ff5b662865cd10c63da1be5ab74de58864198192
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-03-12 14:54:31 +0530

    03-12-2026 2576-find-the-maximum-number-of-marked-indices .cpp Medium

--- 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.cpp
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int maxNumOfMarkedIndices(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int i = 0, j = (n + 1) / 2;
-        int count = 0;
-        while (i < (n + 1) / 2 && j < n) {
-            if (2 * nums[i] <= nums[j]) {
-                count += 2;
-                i++;
-                j++;
-            } else {
-                j++;
+        set<int> st;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=nums.size()/2;j<nums.size();j++)
+            {
+                if(2*nums[i] <=nums[j] && st.find(i) ==st.end() && st.find(j) ==st.end())
+                {
+                    st.insert(i);
+                    st.insert(j);
+                    break;
+                }
             }
         }
-        return count;
+        return st.size();
     }
 };
