b1217a74dc298a23be09f3786b7cd966c8e77b5e
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-06-13 22:42:12 +0530

    06-13-2026 3838-weighted-word-mapping .cpp Easy

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    string mapWordWeights(vector<string>& wr, vector<int>& wg) {
+    string mapWordWeights(vector<string>& wo, vector<int>& w) {
         string st="";
-        for(int i=0;i<wr.size();i++)
-        {   
+        for(int i=0;i<wo.size();i++)
+        {    
             int s=0;
-            for(int j=0;j<wr[i].size();j++)
-            {
-                s=s+wg[wr[i][j]-'a'];
+            for(int j=0;j<wo[i].size();j++)
+            {   
+                s=s+w[wo[i][j]-'a'];
             }
             int k=s%26;
-            st=st+char('a'+(25-k));
+            st=st+char('z'-k);
         }
         return st;
         
