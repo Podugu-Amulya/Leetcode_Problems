4f83b06db0c3262bd4742ee3e6d4ae972df4dc29
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-06-15 20:04:49 +0530

    06-15-2026 3945-digit-frequency-score .cpp Easy

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        map<int,int> mp;
+      //  map<int,int> mp;
+      int s=0;
         while(n!=0)
         {
             int rem=n%10;
-            mp[rem]++;
+           // mp[rem]++;
+           s=s+rem;
             n=n/10;
         }
-        int s=0;
-        for(auto& [k,v]:mp)
-        {
-            s=s+(k*v);
-        }
+        // int s=0;
+        // for(auto& [k,v]:mp)
+        // {
+        //     s=s+(k*v);
+        // }
         return s;
         
     }
