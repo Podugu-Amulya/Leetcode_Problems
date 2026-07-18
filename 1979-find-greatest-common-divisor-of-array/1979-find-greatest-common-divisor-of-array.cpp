9ee21436e521f66101821abc4da52b7f264ff314
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-07-18 09:39:30 +0530

    07-18-2026 1979-find-greatest-common-divisor-of-array .cpp Easy

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -1,28 +1,9 @@
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int f=nums.front();
-        int l=nums.back();
-        set<int> st1;
-        set<int> st2;
-        for(int i=1;i<=f;i++)
-        {
-            if (f%i==0)
-            st1.insert(i);
-        }
-        for(int i=1;i<=l;i++)
-        {
-            if (l%i==0)
-            st2.insert(i);
-        }
-        vector<int> result;
-        set_intersection(
-        st1.begin(), st1.end(),
-        st2.begin(), st2.end(),
-        back_inserter(result)
-    );
-       int k= *std::max_element(result.begin(),result.end());
-       return k;
+        int a=*min_element(nums.begin(),nums.end());
+        int b=*max_element(nums.begin(),nums.end());
+        return std::gcd(a,b);
+
     }
 };
