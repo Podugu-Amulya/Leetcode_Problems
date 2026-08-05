e03cd8088d3e9864a7a76cd6851b14ab077774d9
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-08-05 13:54:50 +0530

    08-05-2026 0680-valid-palindrome-ii .cpp Easy

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool pal(string s,int l,int r)
+    {
+
+        while(l<=r)
+        {
+            if(s[l]!=s[r])
+            {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int l=0;
+        int r=s.size()-1;
+        while(l<r)
+        {
+            if(s[l]!=s[r])
+            {
+                return pal(s,l+1,r) || pal(s,l,r-1);
+            }
+            else
+            {
+                l++;
+                r--;
+            }
+        }
+        return true;
+        
+    }
+};
