84b4b6b950369180693a8c8769b9fde4b4016577
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-08-07 10:54:20 +0530

    08-07-2026 0209-minimum-size-subarray-sum .cpp Medium

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int minSubArrayLen(int t, vector<int>& nums) {
-        int r=0;
         int l=0;
+        
+        int n=nums.size();
         int m=INT_MAX;
-        long long s=0;
-        for(int r=0;r<nums.size();r++)
+        int s=0;
+        
+        for(int r=0;r<n;r++)
         {
-             s=s+nums[r];
-             while(s >= t)
-             {
-                m=min(m,(r-l+1));
+            s=s+nums[r];
+            while(s>=t)
+            {   m=min(m,(r-l+1));
                 s=s-nums[l];
                 l++;
-             }
+            }
         }
-        return m==INT_MAX? 0:m;
+        return m==INT_MAX? 0 :m;
     }
 };
