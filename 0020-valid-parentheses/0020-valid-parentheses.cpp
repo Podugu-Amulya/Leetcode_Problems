6bd25611bdd55b3cdc35959c84e45c3736391a39
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-08-08 11:32:41 +0530

    08-08-2026 0020-valid-parentheses .cpp Easy

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++)
+        {  
+            char ch=s[i];
+            if(ch=='(' || ch=='{' || ch=='[')
+            {
+                st.push(ch);
+            }
+            else if(ch==')' && !st.empty() && st.top()=='(')
+            {
+                st.pop();
+            }
+            else if(ch==']' && !st.empty() && st.top()=='[')
+            {
+                st.pop();
+            }
+            else if(ch=='}' && !st.empty() && st.top()=='{')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(ch);
+            }
+        }
+        return st.empty();
+    }
+};
