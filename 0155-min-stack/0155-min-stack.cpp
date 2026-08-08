f1bcff7932be2f44afe98cda65efd40370e5c9c2
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-08-08 14:08:39 +0530

    08-08-2026 0155-min-stack .cpp Medium

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,31 @@
+#include <stack>
+using namespace std;
+
+class MinStack {
+public:
+    stack<pair<int, int>> st;
+
+    MinStack() {
+    }
+
+    void push(int val) {
+        if (st.empty()) {
+            st.push({val, val});
+        } else {
+            int Min = min(val, st.top().second);
+            st.push({val, Min});
+        }
+    }
+
+    void pop() {
+        st.pop();
+    }
+
+    int top() {
+        return st.top().first;
+    }
+
+    int getMin() {
+        return st.top().second;
+    }
+};
