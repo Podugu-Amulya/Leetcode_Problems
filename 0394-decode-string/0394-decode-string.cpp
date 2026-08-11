9a0a33c571a3d80487279a6b3405f4b03fc78328
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-08-11 10:33:14 +0530

    08-11-2026 0394-decode-string .cpp Medium

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> nums;
+        stack<string> st;
+
+        string curr = "";
+        int num = 0;
+
+        for (char ch : s) {
+
+            if (isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            else if (ch == '[') {
+                nums.push(num);
+                st.push(curr);
+
+                num = 0;
+                curr = "";
+            }
+
+            else if (ch == ']') {
+                int k = nums.top();
+                nums.pop();
+
+                string temp = curr;
+
+                curr = st.top();
+                st.pop();
+
+                for (int i = 0; i < k; i++) {
+                    curr += temp;
+                }
+            }
+
+            else {
+                curr += ch;
+            }
+        }
+
+        return curr;
+    }
+};
