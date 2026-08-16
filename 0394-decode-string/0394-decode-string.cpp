48c6f10b99454293819cda4ab971c50bbf670992
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-08-16 08:29:53 +0530

    08-16-2026 0394-decode-string .cpp Medium

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
+
     string decodeString(string s) {
-        stack<int> nums;
-        stack<string> st;
-
-        string curr = "";
-        int num = 0;
-
-        for (char ch : s) {
-
-            if (isdigit(ch)) {
-                num = num * 10 + (ch - '0');
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != ']') {
+                st.push(s[i]);
             }
-
-            else if (ch == '[') {
-                nums.push(num);
-                st.push(curr);
-
-                num = 0;
-                curr = "";
-            }
-
-            else if (ch == ']') {
-                int k = nums.top();
-                nums.pop();
-
-                string temp = curr;
-
-                curr = st.top();
-                st.pop();
-
-                for (int i = 0; i < k; i++) {
-                    curr += temp;
+            else{
+                string curr_str = "";
+                
+                while(st.top() != '['){
+                    curr_str = st.top() + curr_str ;
+                    st.pop();
+                }
+                
+                st.pop();   // for '['
+                string number = "";
+                
+              
+                
+                while(!st.empty() && isdigit(st.top())){
+                    number = st.top() + number;
+                    st.pop();
+                }
+                int k_time = stoi(number);   
+                
+                while(k_time--){
+                    for(int p = 0; p < curr_str.size() ; p++)
+                        st.push(curr_str[p]);
                 }
             }
-
-            else {
-                curr += ch;
-            }
         }
-
-        return curr;
+        
+        s = "";
+        while(!st.empty()){
+            s = st.top() + s;
+            st.pop();
+        }
+        return s;
+        
     }
 };
