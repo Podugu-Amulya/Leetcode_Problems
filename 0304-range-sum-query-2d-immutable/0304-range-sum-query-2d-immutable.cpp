644865b6f423841b4e0ddfc4b2d0d1c594678215
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-08-16 08:25:36 +0530

    08-16-2026 0304-range-sum-query-2d-immutable .cpp Medium

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,45 +1,26 @@
 class NumMatrix {
 public:
-    vector<vector<int>> ans;
-
+    vector<vector<int>> prefix;
     NumMatrix(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-
-        ans.resize(n, vector<int>(m));
-
-        ans[0][0] = matrix[0][0];
-
-        for (int j = 1; j < m; j++) {
-            ans[0][j] = ans[0][j - 1] + matrix[0][j];
-        }
-
-        for (int i = 1; i < n; i++) {
-            ans[i][0] = ans[i - 1][0] + matrix[i][0];
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                ans[i][j] = matrix[i][j]
-                          + ans[i - 1][j]
-                          + ans[i][j - 1]
-                          - ans[i - 1][j - 1];
+        prefix.resize(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                prefix[i + 1][j + 1] = matrix[i][j] + prefix[i + 1][j] +
+                                       prefix[i][j + 1] - prefix[i][j];
             }
         }
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int res = ans[row2][col2];
-
-        if (row1 > 0)
-            res -= ans[row1 - 1][col2];
-
-        if (col1 > 0)
-            res -= ans[row2][col1 - 1];
-
-        if (row1 > 0 && col1 > 0)
-            res += ans[row1 - 1][col1 - 1];
-
-        return res;
+        return (prefix[row2 + 1][col2 + 1] + prefix[row1][col1] -
+                prefix[row2 + 1][col1] - prefix[row1][col2 + 1]);
     }
 };
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
