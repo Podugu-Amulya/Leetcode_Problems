d87e185a15efbdef8c943952b816bcb555af6a2b
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-08-18 10:00:33 +0530

    08-18-2026 0234-palindrome-linked-list .cpp Easy

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+          if(head==nullptr || head->next==nullptr)
+          {
+            return true;
+          }
+          ListNode* slow=head;
+          ListNode* fast=head;
+          while(fast!=nullptr && fast->next!=nullptr)
+          {
+            slow=slow->next;
+            fast=fast->next->next;
+          }
+
+           ListNode* prev=nullptr;
+           while(slow !=nullptr)
+           {
+            ListNode* front=slow->next;
+            slow->next=prev;
+            prev=slow;
+            slow=front;
+           }
+           while(prev!=nullptr)
+           {
+            if(prev->val!=head->val)
+            {
+                return false;
+            }
+            prev=prev->next;
+            head=head->next;
+           }
+           return true;
+
+           
+    }
+};
