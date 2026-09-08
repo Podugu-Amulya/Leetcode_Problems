ac934abacd2127f73377e4075817947d940b87ff
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-09-08 10:59:47 +0530

    09-08-2026 3870-count-commas-in-range .cpp Easy

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int countCommas(int n) {
-        string st=to_string(n);
-        if(st.size()<=3)
+        if(n<1000)
         {
             return 0;
         }
-        return (n-1000)+1;
+        else
+        {
+            return n-999;
+        }
         
     }
 };
