a5a2392b0f2a783c4663b6dee12a2b6ca0c308dd
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-09-20 11:06:30 +0530

    09-20-2026 0056-merge-intervals .cpp Medium

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& inte) {
-        sort(inte.begin(),inte.end());
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
         vector<vector<int>> res;
-        res.push_back(inte[0]);
-        
-        for(int i=1;i<inte.size();i++)
-        {
-            if(inte[i][0] <=res.back()[1])
+        sort(nums.begin(),nums.end());
+        res.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {   
+            if(res.back()[0] <=nums[i][1] && nums[i][0] <= res.back()[1])
             {
-                res.back()[1]=max(res.back()[1],inte[i][1]);
-                res.back()[0]=min(res.back()[0],inte[i][0]);
+                res.back()[1]=max(nums[i][1],res.back()[1]);
             }
             else
             {
-                res.push_back(inte[i]);
+                res.push_back(nums[i]);
             }
         }
-       return res;
+        return res;
+        
     }
 };
