f5e01b25eff498f84c2c616a4b3fbe02d8ecee4f
PODUGU AMULYA <23A91A05C0@aec.edu.in>
2026-09-25 11:33:32 +0530

    09-25-2026 0003-longest-substring-without-repeating-characters .cpp Medium

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        map<char,int> mp;
         int l=0;
         int r=0;
         int m=0;
-        map<int,int> mp;
-        while(r < s.size())
-        {   
-
+        while(r<s.size())
+        {
             mp[s[r]]++;
-            if(mp[s[r]]==1)
-            {   
-                m=max(m,(r-l+1));
-            }
-            while(mp[s[r]] >1 )
+            while(mp[s[r]]>1)
             {
                 mp[s[l]]--;
                 if(mp[s[l]]==0)
                 {
-                    mp.erase(s[l]);
-                    
+                    mp.erase(mp[s[l]]);
                 }
                 l++;
-            
-                
             }
+            m=max(m,(r-l+1));
             r++;
-
         }
         return m;
-        
     }
 };
